f5ea83572483b3b53234e887d59ed2f185580b1a
CrossCheque <71478767+CrossCheque@users.noreply.github.com>
2021-11-24 17:44:53 -0500

    Fixes
    Removed @client.event - discord.py is not accepting the command.
    
    Fixed stop command typo.
    Fixed bot.run token misconfiguration.
    Added dotenv library - may be unnecessary after pip3 install locally.
    
    BUG: stop command breaks the bot and doesn't shut down the jvm.
    
    BUG: Subprocess command is taking over the bot shell and directory, needs to be run independently and on its own turf.

--- discraft.py
+++ discraft.py
@@ -1,25 +1,22 @@
 #discraft.py
 import os
 import discord
+import dotenv #import environment file control for TOKEN
 import subprocess #import python utility for shell execution
 from discord.ext import commands
 from dotenv import load_dotenv
 
-client = discord.Client()
+#client = discord.Client()
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix="!")
 
-@client.event
-async def on_ready():
-        print(f'{client.user} has connected to Discord!')
-        
-
-#Open server shell
-#os.system(screen -p minecraft -X vanillaServer) 
-#need to test window names, vanillaServer should be same as .sh
+#@client.event
+#async def on_ready():
+#        print(f'{client.user} has connected to Discord!')
+#THIS BIT NO WORKIE :<
 
 ## COMMANDS ##
 @bot.command(
@@ -27,15 +24,15 @@ async def on_ready():
 	brief="Starts the server."
 )
 async def start(ctx):
-	subprocess.call(['sh', './Users/harbinger/Downloads/Minecraft/vanillaserver.sh'])
+	subprocess.call(['sh', '/Users/harbinger/Downloads/Minecraft/vanillaserver.sh'])
 #bot calls OS to start new shell
 
 @bot.command(
 	help="Terminates active Minecraft server, hopefully gracefully.",
 	brief="Stops the server."
 )
-async def start(ctx):
-	os.system(screen -p minecraft -X vanillaServer "stop\015")
+async def stop(ctx):
+	os.system("screen -S minecraft -p 0 -X vanillaserver 'stop\015'")
 #bot forwards STOP command to minecraft shell
 
-bot.run(DISCORD_TOKEN)
+bot.run(TOKEN)
