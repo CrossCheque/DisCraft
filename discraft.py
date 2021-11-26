6d8254ce68aae79636657da5dbf586a96f6f7878
CrossCheque <71478767+CrossCheque@users.noreply.github.com>
2021-11-25 20:28:49 -0500

    Update Start
    Reconfigured start command to use os.system in place of subprocess.

--- discraft.py
+++ discraft.py
@@ -6,25 +6,22 @@ import subprocess #import python utility for shell execution
 from discord.ext import commands
 from dotenv import load_dotenv
 
-#client = discord.Client()
-
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix="!")
 
-#@client.event
-#async def on_ready():
-#        print(f'{client.user} has connected to Discord!')
-#THIS BIT NO WORKIE :<
-
 ## COMMANDS ##
 @bot.command(
-	help="Inovkes the server start shell.",
+	help="Start a server.",
 	brief="Starts the server."
 )
 async def start(ctx):
-	subprocess.call(['sh', '/Users/harbinger/Downloads/Minecraft/vanillaserver.sh'])
+#	subprocess.call(['sh', '/servers/vanilla/vanillaserver.sh'])
+	os.system('screen -dmS vanillaserver')
+	os.system('screen -rS vanillaserver')
+	os.system('cd "./servers/vanilla"')
+	os.system('./vanillaserver.sh')
 #bot calls OS to start new shell
 #change this to a screen command, will fix issue with muliplexing the discraft directory and shell. -AV
 
