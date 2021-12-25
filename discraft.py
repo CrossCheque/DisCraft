63d6baf057a00aee08fb460969443162fd5431a1
CrossCheque <71478767+CrossCheque@users.noreply.github.com>
2021-12-25 03:03:30 -0800

    Update Ignored

--- discraft.py
+++ discraft.py
@@ -1,7 +1,6 @@
 #discraft.py
 import os
 import discord
-import dotenv #import environment file control for TOKEN
 import subprocess #import python utility for shell execution
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -17,11 +16,7 @@ bot = commands.Bot(command_prefix="!")
 	brief="Starts the server."
 )
 async def start(ctx):
-#	subprocess.call(['sh', '/servers/vanilla/vanillaserver.sh'])
-	os.system('screen -dmS vanillaserver')
-	os.system('screen -rS vanillaserver')
-	os.system('cd "./servers/vanilla"')
-	os.system('./vanillaserver.sh')
+	os.system("")
 #bot calls OS to start new shell
 #change this to a screen command, will fix issue with muliplexing the discraft directory and shell. -AV
 
